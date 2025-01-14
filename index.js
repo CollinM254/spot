01a8b63c4c38a4cde86bdaf68020be432bc83021
CollinM254 <cmwenda20@gmail.com>
2025-01-14 22:34:54 +0300

    front

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 const allowedOrigins = ['http://localhost:8080', 
                         'http://localhost:3000', 
                         `http://192.168.1.104:3000`,
-                        // 'https://urbankicks.onrender.com', 
+                        'https://spottrend-nine.vercel.app', 
                         // 'https://urbankicksatserver.onrender.com', 
                         // 'https://urbankicks.netlify.app'
                     ];
