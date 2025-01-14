9d69de0c5136ecdd190f3d14d893f1758992d114
CollinM254 <cmwenda20@gmail.com>
2025-01-14 23:50:59 +0300

    db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 // Debug: Log the DB_PATH to check it's loaded correctly
 console.log("DB_PATH:", process.env.DB_PATH);
 
-mongoose.connect(process.env.DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Time in milliseconds to wait for the server to respond
+    socketTimeoutMS: 20000, // Keep sockets open for 20 seconds; increase if necessary
+    bufferCommands: false, // Disable buffering; not recommended for production unless necessary
+};
+
+mongoose.connect(process.env.DB_PATH, options)
     .then(() => console.log("Connection to database was successful"))
     .catch(err => {
         console.error("Connection error: ", err.message);
     });
+
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// // Debug: Log the DB_PATH to check it's loaded correctly
+// console.log("DB_PATH:", process.env.DB_PATH);
+
+// mongoose.connect(process.env.DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("Connection to database was successful"))
+//     .catch(err => {
+//         console.error("Connection error: ", err.message);
+//     });
 
 // const mongoose = require("mongoose");
 // require("dotenv").config();
